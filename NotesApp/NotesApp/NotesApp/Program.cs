80943a2466aa30799eb6fc96028c63d43f60b293
Marija Petrevska <marija_petrevska25@yahoo.com>
2026-01-06 13:37:04 +0100

    New project

--- NotesApp/NotesApp/NotesApp/Program.cs
+++ NotesApp/NotesApp/NotesApp/Program.cs
@@ -1,3 +1,6 @@
+using Avenga.NotesApp.Helpers;
+using NotesApp.Helpers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,20 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//read from appSettings.Json, find the property AppSettings from the main Object
+var appSettings = builder.Configuration.GetSection("AppSettings");
+builder.Services.Configure<AppSettings>(appSettings);
+AppSettings appSettingsObject = appSettings.Get<AppSettings>();
+
+//Dependency Injection
+DependencyInjectionHelper.InjectDbContext(builder.Services, appSettingsObject.ConnectionString);
+DependencyInjectionHelper.InjectRepositories(builder.Services);
+DependencyInjectionHelper.InjectDapperRepositories(builder.Services, appSettingsObject.ConnectionString);
+//DependencyInjectionHelper.InjectDapperRepositories(builder.Services, "Server=.;Database=NotesAppDatabase;Trusted_Connection=True;TrustServerCertificate=True");
+DependencyInjectionHelper.InjectAdoRepositories(builder.Services, appSettingsObject.ConnectionString);
+DependencyInjectionHelper.InjectServices(builder.Services);
+
 
 var app = builder.Build();
 
